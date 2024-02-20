7e468f7bcb382a833636a82a8d5888eb9599842b
alamfatima1999 <alamfatima1999@gmail.com>
2024-02-20 22:50:53 +0530

    Daily Solution

---
+++ Bit Manipulation/Missing Number.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+        int missing = 0;
+
+        for (int i = 0; i < n; ++i)
+        { // O(n) time complexity
+            missing ^= (i + 1) ^ nums[i];
+        }
+
+        return missing;
+    }
+};
